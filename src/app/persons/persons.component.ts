edf952c3cb7628323a04e7de9b1a776d3cc39abf
SztGellert <sztankgellert@gmail.com>
2023-11-28 05:44:15 +0100

    implement rxJS subscriptions

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {PersonsService} from "./persons.service";
+import {Subscription} from "rxjs";
 
 
 @Component({
@@ -10,14 +11,23 @@ import {PersonsService} from "./persons.service";
   templateUrl: './persons.component.html',
   styleUrl: './persons.component.css'
 })
-export class PersonsComponent implements OnInit{
+export class PersonsComponent implements OnInit, OnDestroy {
   personList: string[] = [];
+  private personListSubs: Subscription = new Subscription;
 
   constructor(private prsService: PersonsService) {
   }
 
   ngOnInit() {
     this.personList = this.prsService.persons;
+    this.personListSubs = this.prsService.personsChanged.subscribe(persons => {
+      this.personList = persons;
+      }
+    );
+  }
+
+  ngOnDestroy() {
+   this.personListSubs.unsubscribe();
   }
 
   onRemovePerson(personName: string) {
